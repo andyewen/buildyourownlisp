2334febb7704008f0738b644133fa29ac6414340
Andrew Ewen <andy1633@hotmail.co.uk>
2016-11-19 17:43:52 +0000

    Progress on s-expression parsing support.

--- parsing.c
+++ parsing.c
@@ -36,7 +36,7 @@ lval* lval_sym(char* s) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_SYM;
   v->data.sym = malloc(strlen(s) + 1);
-  strcpy(v->sym, s);
+  strcpy(v->data.sym, s);
   return v;
 }
 
@@ -56,6 +56,14 @@ lval* lval_sexpr(void) {
   return v;
 }
 
+lval* lval_add(lval* v, lval* x) {
+  v->data.sexprs.count++;
+  v->data.sexprs.cell = realloc(v->data.sexprs.cell,
+                                sizeof(lval*) * v->data.sexprs.count);
+  v->data.sexprs.cell[v->data.sexprs.count - 1] = x;
+  return v;
+}
+
 void lval_del(lval* v) {
   switch (v->type) {
     case LVAL_NUM: break;
@@ -69,7 +77,7 @@ void lval_del(lval* v) {
         lval_del(v->data.sexprs.cell[i]);
       }
       /* Free pointer list. */
-      free v->data.sexprs.cell;
+      free(v->data.sexprs.cell);
       break;
   }
   /* Free the lval struct itself. */
@@ -78,27 +86,54 @@ void lval_del(lval* v) {
 
 
 /* Output */
-void lval_print(lval v) {
-  switch (v.type) {
-    case LVAL_NUM:
-      printf("%li", v.data.num);
-      break;
+void lval_expr_print(lval* v, char open, char close) {
+  putchar(open);
+  for (int i = 0; i < v->data.count, i++) {
+    lval_print(v->data.cell[i]);
+    if (i != (v->data.count - 1)) {
+      putchar(" ");
+    }
+  }
+  putchar(close);
+}
 
-    case LVAL_ERR:
-      if (v.data.err == LERR_DIV_ZERO) {
-        printf("Error: Division by zero.");
-      }
-      if (v.data.err == LERR_BAD_OP) {
-        printf("Error: Invalid operator.");
-      }
-      if (v.data.err == LERR_BAD_NUM) {
-        printf("Error: Invalid number.");
-      }
-      break;
+void lval_print(lval* v) {
+  switch (v.type) {
+    case LVAL_NUM: printf("%li", v->data.num); break;
+    case LVAL_ERR: printf("Error: %s", v->data.err); break;
+    case LVAL_SYM: printf("%s", v->data.sym); break;
+    case LVAL_SEXPR: lval_expr_print(v, "{", "}"); break;
   }
 }
 
 void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ?
+    lval_num(x) : lval_err("Invalid number.");
+}
+
+lval_read(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0 { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0 { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0 { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0 { continue; }
+    if (strcmp(t->children[i]->tag, "regex") == 0 { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+  return x;
+}
 
 
 /* Evaluation logic. */
@@ -203,8 +238,11 @@ int main(int argc, char** argv) {
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      lval result = eval(r.output);
-      lval_println(result);
+      /* lval result = eval(r.output); */
+      /* lval_println(result); */
+      lval* x = lval_read(r.output);
+      lval_println(x);
+      lval_del(x);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
