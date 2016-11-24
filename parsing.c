db891db49c7f80e0ef45a80370e021badec6e8cc
Andrew Ewen <andy1633@hotmail.co.uk>
2016-11-24 02:57:28 +0000

    Add missing "cons" function to startup environment.

--- parsing.c
+++ parsing.c
@@ -496,6 +496,7 @@ void lenv_add_builtins(lenv* e) {
   lenv_add_builtin(e, "tail", builtin_tail);
   lenv_add_builtin(e, "eval", builtin_eval);
   lenv_add_builtin(e, "join", builtin_join);
+  lenv_add_builtin(e, "cons", builtin_cons);
   lenv_add_builtin(e, "len",  builtin_len);
   lenv_add_builtin(e, "init", builtin_init);
 
